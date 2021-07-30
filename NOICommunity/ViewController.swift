c0c449217c6b04202e6a925e79eece0d7cd06e54
Matteo Matassoni <4108197+matax87@users.noreply.github.com>
2021-07-30 12:16:46 +0200

    Create Xcode project

---
+++ NOICommunity/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NOI Community
+//
+//  Created by Matteo Matassoni on 30/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
