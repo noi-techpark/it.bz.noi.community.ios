d356fc7054a33ed7e4bc87071f0f2e28de939cbc
Peter Moser <p.moser@noi.bz.it>
2021-08-10 21:13:42 +0200

    WIP

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -19,5 +19,3 @@ security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
 security unlock-keychain -p "" ~/Library/Keychains/build.keychain
 security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
-
-view raw
