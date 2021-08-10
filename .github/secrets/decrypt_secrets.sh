db501d6edf0b003149b679b97344c4fc3e77cef5
Peter Moser <p.moser@noi.bz.it>
2021-08-10 20:50:15 +0200

    WIP

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -6,15 +6,21 @@ MOBILEPROV_NAME="itbznoicommunity.mobileprovision"
 CERTIFICATE_NAME="ios_distribution.p12"
 SECRETS_PATH=".github/secrets"
 
+echo "DECRYPT FILES"
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEY_PASSPHRASE" --output $SECRETS_PATH/$MOBILEPROV_NAME $SECRETS_PATH/$MOBILEPROV_NAME.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEY_PASSPHRASE" --output $SECRETS_PATH/$CERTIFICATE_NAME $SECRETS_PATH/$CERTIFICATE_NAME.gpg
 
-mkdir -p "$HOME/Library/MobileDevice/Provisioning Profiles"
+mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles/
 
-cp $SECRETS_PATH/$MOBILEPROV_NAME "$HOME/Library/MobileDevice/Provisioning Profiles/$MOBILEPROV_NAME"
+echo "List profiles"
+ls ~/Library/MobileDevice/Provisioning\ Profiles/
+echo "Move profiles"
+cp $SECRETS_PATH/*.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
+echo "List profiles"
+ls ~/Library/MobileDevice/Provisioning\ Profiles/
 
 security create-keychain -p "" build.keychain
-security import $SECRETS_PATH/$CERTIFICATE_NAME -t agg -k ~/Library/Keychains/build.keychain -P "" -A
+security import $SECRETS_PATH/$CERTIFICATE_NAME -t agg -k ~/Library/Keychains/build.keychain -P "$IOS_KEY_PASSPHRASE" -A
 
 security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
@@ -22,4 +28,4 @@ security unlock-keychain -p "" ~/Library/Keychains/build.keychain
 
 security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
 
-view raw
+# view raw
