1ab2228d89227a7c61715ade0c589cf6ed406b98
Peter Moser <p.moser@noi.bz.it>
2021-08-11 10:03:13 +0200

    WIP

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -2,7 +2,7 @@
 
 set -xeo pipefail
 
-MOBILEPROV_NAME="itbznoicommunity.mobileprovision"
+MOBILEPROV_NAME="it.bz.noi.community.mobileprovision"
 CERTIFICATE_NAME="ios_distribution.p12"
 SECRETS_PATH=".github/secrets"
 
@@ -11,7 +11,7 @@ gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEY_PASSPHRASE" --output 
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEY_PASSPHRASE" --output $SECRETS_PATH/$CERTIFICATE_NAME $SECRETS_PATH/$CERTIFICATE_NAME.gpg
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles/
-cp $SECRETS_PATH/*.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
+cp $SECRETS_PATH/$MOBILEPROV_NAME ~/Library/MobileDevice/Provisioning\ Profiles/
 
 security create-keychain -p "" build.keychain
 security import $SECRETS_PATH/$CERTIFICATE_NAME -t agg -k ~/Library/Keychains/build.keychain -P "$IOS_KEY_PASSPHRASE" -A
