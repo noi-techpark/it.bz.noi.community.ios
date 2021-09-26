bf00c76bebd64d06c68543e76c33bdc7213d8ba5
Matteo Matassoni <4108197+matax87@users.noreply.github.com>
2021-09-26 19:32:47 +0200

    Adds meet feature

--- NOICommunity/Extensions/URL+KnowNOIURLs.swift
+++ NOICommunity/Extensions/URL+KnowNOIURLs.swift
@@ -10,4 +10,11 @@ import Foundation
 extension URL {
     static let map = URL(string: .localized("url_map"))!
     static let roomBooking = URL(string: .localized("url_room_booking"))!
+    static let urlCompanies = URL(string: .localized("url_companies"))!
+    static let urlStartups = URL(string: .localized("url_startups"))!
+    static let urlUniversity = URL(string: .localized("url_university"))!
+    static let urlResearch = URL(string: .localized("url_research"))!
+    static let urlInstitutions = URL(string: .localized("url_institutions"))!
+    static let urlLab = URL(string: .localized("url_lab"))!
+    static let urlAboutUs = URL(string: .localized("url_about_us"))!
 }
