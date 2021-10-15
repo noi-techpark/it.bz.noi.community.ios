a4f18278cfdd05461a634defcfab35ec06820891
Matteo Matassoni <4108197+matax87@users.noreply.github.com>
2021-10-15 16:34:31 +0200

    Integrates Firebase Crashlytics (only on release builds)

--- NOICommunity/AppDelegate.swift
+++ NOICommunity/AppDelegate.swift
@@ -6,12 +6,18 @@
 //
 
 import UIKit
+#if RELEASE
+import Firebase
+#endif
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+#if RELEASE
+        FirebaseApp.configure()
+#endif
         configureAppearances()
 
         return true
