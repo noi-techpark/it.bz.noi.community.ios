2aa3d570425694c3e0d3d29ec8153bd563dedde8
Matteo Matassoni <4108197+matax87@users.noreply.github.com>
2022-05-17 13:02:47 +0200

    Improves Events error handling

---
+++ NOICommunity/Extensions/MessageViewController+Error.swift
@@ -0,0 +1,42 @@
+//
+//  MessageViewController+Error.swift
+//  NOICommunity
+//
+//  Created by Matteo Matassoni on 17/05/22.
+//
+
+import Foundation
+
+extension MessageViewController {
+    
+    convenience init(error: Error) {
+        let message: Message
+        
+        if let localizedError = error as? LocalizedError,
+            let errorDescription = localizedError.errorDescription {
+            let detailedTextComponents = [
+                localizedError.failureReason,
+                localizedError.recoverySuggestion
+            ]
+                .compactMap { $0 }
+            
+            message = .init(
+                text: errorDescription,
+                detailedText: detailedTextComponents.isEmpty
+                ? nil
+                : detailedTextComponents.joined(separator: "\n\n"),
+                image: nil,
+                action: nil)
+        } else {
+            message = Message(
+                text: error.localizedDescription,
+                detailedText: nil,
+                image: nil,
+                action: nil
+            )
+        }
+
+        self.init(message: message)
+    }
+    
+}
