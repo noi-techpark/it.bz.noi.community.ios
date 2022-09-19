be7d5adf283c9dd01f3c03e1a19ee56c6dbb26a5
Matteo Matassoni <4108197+matax87@users.noreply.github.com>
2022-09-19 15:23:29 +0200

    Adds white outline on yellow circle of event date in the events list and event detail cards

--- NOICommunity/Views/CircleView.swift
+++ NOICommunity/Views/CircleView.swift
@@ -8,15 +8,33 @@
 import UIKit
 
 @IBDesignable class CircleView: UIView {
-
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor {
+        get {
+            UIColor(cgColor: layer.borderColor!)
+        }
+        set {
+            layer.borderColor = newValue.cgColor
+        }
+    }
+    
     override func layoutSubviews() {
         super.layoutSubviews()
-
+        
         let edge = min(frame.width, frame.height)
         layer.cornerRadius = edge / 2
         layer.masksToBounds = true
     }
-
+    
     override func prepareForInterfaceBuilder() {
         super.prepareForInterfaceBuilder()
         setNeedsLayout()
