f8c89c9a17c30fafc979ef70fa9949edcb5b4f05
henri <henri@moosbauer.com>
2023-06-05 10:04:54 +0200

    fix missing version specifier

--- NOICommunityLib/Package.swift
+++ NOICommunityLib/Package.swift
@@ -1,9 +1,9 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 // SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
-
-// swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
