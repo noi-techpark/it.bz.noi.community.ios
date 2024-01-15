1e783a5524c3d72d8aa6803795dc9faa3ebe0afb
Matteo Matassoni <4108197+matax87@users.noreply.github.com>
2024-01-15 17:41:45 +0100

    Removes address information and find call to action from person details screen

---
+++ NOICommunity/Helpers/FeatureFlag.swift
@@ -0,0 +1,16 @@
+// SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+//
+//  FeatureFlag.swift
+//  NOICommunity
+//
+//  Created by Matteo Matassoni on 15/01/24.
+//
+
+import Foundation
+
+enum FeatureFlag {
+    
+}
